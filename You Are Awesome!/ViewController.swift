70a15d5750b6b6820d8bd5250d10291f47a25813
Andrew <andrewevangelista@andreweistasmbp.hsd1.ma.comcast.net>
2019-01-22 22:51:35 -0500

    2.10 Completed!

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+    
     
     //Code below executes when the view loads
     override func viewDidLoad() { //Comment 2
@@ -22,19 +24,41 @@ class ViewController: UIViewController {
 
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let message1 = "You Are Awesome!"
-        let message2 = "You Are Great!"
-        let message3 = "You Are Amazing!"
         
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "When the Genius Bar Needs Help, They Call You",
+                        "You Brighten My Day!",
+                        "You are da bomb!",
+                        "Hey, Fabulous!",
+                        "You Are Tremendous!",
+                        "You've Got the Design Skills of Jony Ive!",
+                        "I Can't Wait to Download Your App"]
         
-        if messageLabel.text == message1 {
-            messageLabel.text = message2
-        } else if messageLabel.text == message2 {
-            messageLabel.text = message3
+        messageLabel.text = messages[index]
+        
+        if index == messages.count - 1 {
+            index = 0
         }else {
-            messageLabel.text = message1
-             
-        }
+        
+        index = index + 1
+        
+        
+        
+//        let message1 = "You Are Awesome!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        }else {
+//            messageLabel.text = message1
+//
+//        }
         
     }
     
@@ -43,3 +67,4 @@ class ViewController: UIViewController {
     
  }
 
+ }
